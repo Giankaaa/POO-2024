62eeec24ac40ba94623a3489b5196bd55279d234
Giankaaa <161742744+Giankaaa@users.noreply.github.com>
2024-02-29 11:07:21 -0500

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio4;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        double  EDJuan, EDMama, EDAlberto, EDAna;
+        EDJuan = 9;
+        EDAlberto = 2*EDJuan/3;
+        EDAna = 4*EDJuan/3;
+        EDMama = EDJuan + EDAlberto + EDAna;
+        System.out.println("La edad de la mamá de Juan es " + EDMama);
+        System.out.println("La edad de Juan es " + EDJuan);
+        System.out.println("La edad de Alberto " + EDAlberto);
+        System.out.println("La edad de Ana " + EDAna);
+    }
+}
